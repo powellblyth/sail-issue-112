fbc4f779b5ab6cbd4085f14fa5183c0e823e0cfe
Toby Powell-Blyth <tobypowell-blyth@elasticstage.com>
2021-07-12 14:24:21 +0100

    Failure example

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,11 +2,13 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
+    use RefreshDatabase;
     /**
      * A basic test example.
      *
@@ -14,6 +16,9 @@ class ExampleTest extends TestCase
      */
     public function test_example()
     {
+        User::factory(['email'=>'taylor@laravel.com', 'name'=>'Taylor Otwell'])->create();
+        User::factory(['email'=>'dries@laravel.com', 'name'=>'Dries Vints'])->create();
+
         $response = $this->get('/');
 
         $response->assertStatus(200);
